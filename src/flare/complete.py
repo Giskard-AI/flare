0426fcc3e716b3e4275e8b8f33ef7ac4402f635f
Pierre Le Jeune <pierre@giskard.ai>
2025-10-17 18:03:45 +0200

    Clean up

--- src/flare/complete.py
+++ src/flare/complete.py
@@ -28,7 +28,7 @@ async def safe_completion(
         if region is not None:
             region = region.upper().replace("-", "_")
             kwargs["api_key"] = os.getenv(f"AWS_BEARER_TOKEN_BEDROCK_{region}")
-    logger.info(f"Calling completion for model {model_name} with kwargs {kwargs}")
+            
     # TODO: try with models to see how if it's working or not
     while True:
         wait_time = math.ceil(60 + 60 * random.random())
